ece04ed6e1193f82de65ea0c1273e85c56520bee
https://github.com/Devsonar19/My-Notes.git <devsonar19@outlook.com>
2026-01-09 17:15:39 +0530

    day4

--- day2.py
+++ day2.py
@@ -1,11 +1,14 @@
 # #word replacement
-# sentence=input('Enter Your sentence: ')
-# print(sentence)
-# word1=input('Enter your word to replace: ')
-# word2=input('Enter your word to replace it with: ')
-# print(sentence.replace(word1, word2))
+'''
+sentence=input('Enter Your sentence: ')
+print(sentence)
+word1=input('Enter your word to replace: ')
+word2=input('Enter your word to replace it with: ')
+print(sentence.replace(word1, word2))
+'''
 
 #Lists
+'''
 lang=['Java', 'Python', 'C++', 'Dart', 2004, True]  #can also use list(('java',24,False)) [List Constructor]
 print(lang)
 print(lang[0])
@@ -16,7 +19,9 @@ lang[2]='C'
 print(lang)
 print(lang[-1])
 
+'''
 #List Methods
+'''
 list1=[1,3,5,6,7,2,9,5,8,2]
 list2=['rust','ruby','swift','assembly']
 # list1.extend(list2)
@@ -52,3 +57,4 @@ print(list2)
 
 del list2[0]
 print(list2)
+'''
--- day3.py
+++ day3.py
@@ -1,4 +1,6 @@
-#tuples 
+#tuples
+'''
+
 three_numbers=(1,2,3,1,2)
 earth=('water', 'air', 'land')
 combo=('water', 10, True)
@@ -7,26 +9,32 @@ print(combo)
 lang=tuple(('Java', 'Python','C#'))
 print(type(lang))
 
+'''
 #functions
-# def greetings(name, age):
-#     print('Welcome to Python World, '+name+' Your age is '+str(age))
+'''
+def greetings(name, age):
+    print('Welcome to Python World, '+name+' Your age is '+str(age))
 
-# name=input('Enter Name: ')
-# age=int(input('Enter Age: '))
-# greetings(name, age) #calling the functions
+name=input('Enter Name: ')
+age=int(input('Enter Age: '))
+greetings(name, age) #calling the functions
 
-# def checking(*names):
-#     print('Yellow '+names[1])
-# checking('yoii','hoii')
+def checking(*names):
+    print('Yellow '+names[1])
+checking('yoii','hoii')
 
+'''
 # #return keyword
+'''
 def add(num1, num2):
     return num1+num2
 
 print(add(10, 20))
 
 
+'''
 #if statement
+'''
 a=1
 b=2
 
@@ -34,3 +42,4 @@ if b > a or b > 0:  #and
     print(str(b)+' is greater.')
 else:
     print(str(a)+' is greater..')
+'''

5449ac19a63a64c2cca0f79f0f6bdee71bc3266e
https://github.com/Devsonar19/My-Notes.git <devsonar19@outlook.com>
2026-01-16 21:42:26 +0530

    learning POST method, Serializers, using Postman

---
+++ Projects/djangoREST/drfProject/drfProject/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from drfApp.serializers import StudentSerializer
+from drfApp.models import Student
+
+
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+            'username':'admin',
+            'years_active': 10
+        }
+        return Response(data)
+    
+    def post(self, request, *args, **kwargs):
+        serializer = StudentSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
